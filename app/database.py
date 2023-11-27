f48e8842aff53f2af54516d5f57111ca278aeaac
emircanerol <erole20@itu.edu.tr>
2023-11-27 11:29:48 +0300

    Docker files and flask cli for init-db

--- app/database.py
+++ app/database.py
@@ -1,65 +1,23 @@
 import psycopg2
 import click, os
+from flask import current_app, g
 from flask.cli import with_appcontext
 from os import path
 import os
+
+PATH = path.dirname(os.path.abspath(__file__))
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 
 class Database():
 	def __init__(self, app) -> None:
 		self.app = app
 		self.db, self.cur = self.get_db()
 
-	def insertCSV(self, tableCsvPaths: dict):
-		for table, csvPath in tableCsvPaths.items():
-			with open(csvPath, 'r') as f:
-				copyQuery = f'COPY {table} FROM stdin WITH CSV HEADER NULL \'\\N\''
-
-				self.cursor.copy_expert(sql=copyQuery, file=f)
-				self.db.commit()
-				print(f'{table} is inserted!')
-
 	def get_db(self):
 		self.db = psycopg2.connect(self.app.config['DB_URI'])
 		self.cur = self.db.cursor()
 		return self.db, self.cur
-
-	def close_db(self):
-		self.cur.close()
-		self.db.close()
-
-	def init_db(self):
-		with open(path.join('queries', 'schema.sql')) as f:
-			self.cur.execute(f.read())
-			self.db.commit()
-
-		try:
-			PATH = 'data'
-
-			tables = ['drivers', 'constructors', 'circuits', 'races', 'qualifying',
-				'status', 'sprintResults', 'results', 'pitStops', 'lapTimes',
-				'driverStandings', 'constructorStandings', 'constructorResults']
-			
-			tableCsvPaths = { table: os.path.join(PATH, table+'.csv') for table in tables }
-			
-			self.insertCSV(tableCsvPaths)
-
-		except psycopg2.OperationalError as e:
-			print(e)
-			self.db.rollback()
-			self.db.close()
-
-		else:
-			click.echo('Initialized the database.')
-
-	@click.command('init-db')
-	@with_appcontext
-	def init_db_command(self):
-		self.init_db()
-
-	def init_app(self, app):
-		app.teardown_appcontext(self.close_db)
-		app.cli.add_command(self.init_db_command)
 
 	def getRaceResults(self, params):
 		with open(path.join('queries', 'raceResults.sql'), 'r') as f:
@@ -163,3 +121,59 @@ class Database():
 				self.db.rollback()
 				self.db.close()
 		return data
+
+
+def insertCSV(tableCsvPaths: dict, cursor, db):
+	PATHDIR = path.dirname(PATH)
+	for table, csvPath in tableCsvPaths.items():
+		csvPathabs = path.join(PATHDIR, 'data', csvPath)
+		with open(csvPathabs, 'r') as f:
+			copyQuery = f'COPY {table} FROM stdin WITH CSV HEADER NULL \'\\N\''
+
+			cursor.copy_expert(sql=copyQuery, file=f)
+			db.commit()
+			print(f'{table} is inserted!')
+
+def init_db():
+	with open(path.join(PATH, 'queries', 'schema_dev.sql')) as f:
+		query = f.read()
+
+	try:
+		print()
+		db = psycopg2.connect(current_app.config['DB_URI'])
+		
+		cur = db.cursor()
+		cur.execute(query)
+		db.commit()
+
+		tables = ['drivers', 'constructors', 'circuits', 'races', 'qualifying',
+			'status', 'sprintResults', 'results', 'pitStops', 'lapTimes',
+			'driverStandings', 'constructorStandings', 'constructorResults']
+		
+		tableCsvPaths = { table: os.path.join(table+'.csv') for table in tables }
+		
+		insertCSV(tableCsvPaths, cur, db)
+
+	except psycopg2.OperationalError as e:
+		print(e)
+		db.rollback()
+		db.close()
+
+	else:
+		click.echo('Initialized the database.')
+		
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+	init_db()
+
+
+def close_db(e=None):
+	g.db.cur.close()
+	g.db.db.close()
+	g.pop('db', None)
+
+	
+def init_app(current_app):
+	#current_app.teardown_appcontext(close_db)
+	current_app.cli.add_command(init_db_command)
