a9dbcfd4c2b9538974ef3baac9fb1fca996059f9
Yusuf Emir Sezgin <122892888+JustFallenn@users.noreply.github.com>
2023-12-02 21:08:07 +0300

    Delete app directory

--- app/queries/drivers.sql
+++
@@ -1,1 +0,0 @@
-SELECT forename, surname, nationality, number, driverid FROM drivers;
--- app/queries/raceResults.sql
+++
@@ -1,9 +0,0 @@
-SELECT d.forename, d.surname, d.nationality, d.number, c.name || ', ' || c.country, r.year, CONCAT(res.fastestlap, '/', res.laps), res.position, regexp_replace(res.positiontext, '[^a-zA-Z]', '', 'g')
-FROM drivers AS d
-JOIN results AS res USING(driverid)
-JOIN races AS r ON r.raceid = res.raceid AND
-      (r.year = %(raceYear)s OR %(raceYear)s IS NULL)
-JOIN circuits AS c ON c.circuitid = r.circuitid AND
-      (LOWER(c.circuitRef) LIKE %(circuitRef)s OR LOWER(c.name) LIKE %(circuitRef)s OR LOWER(c.country) LIKE %(circuitRef)s OR LOWER(c.location) LIKE %(circuitRef)s)
-ORDER BY (res.position, d.surname)
-LIMIT %(respNumber)s;
