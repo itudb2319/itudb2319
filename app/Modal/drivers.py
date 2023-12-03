f7005008d45080e0002ddc4dd9cfa9bc2c24b068
Can Çalışkan <81326097+canetizen@users.noreply.github.com>
2023-12-03 19:11:15 +0300

    getFilteredDrivers is added for filtering

--- app/Modal/drivers.py
+++ app/Modal/drivers.py
@@ -7,3 +7,17 @@ def getDrivers():
         
         data = db.execute_query(query)
     return data
+    
+def getFilteredDrivers(self, filtered_columns):
+		cond = ', '.join(filtered_columns)
+		header_cond = ", ".join([f"'{item}'" for item in filtered_columns])
+		try:
+			self.cur.execute(f"SELECT driverId, {cond} FROM drivers;")
+			data = self.cur.fetchall()
+
+		except psycopg2.OperationalError as e:
+			click(e)
+			self.conn.rollback()
+			self.conn.close()
+
+		return data
