83e007c9105ffd6d85c8d9420bcf60db86a6ffdc
Can Çalışkan <81326097+canetizen@users.noreply.github.com>
2023-12-03 19:13:22 +0300

    drivers.py is changed for filtering

--- app/Controller/drivers.py
+++ app/Controller/drivers.py
@@ -5,12 +5,37 @@ driversBP = Blueprint('drivers', __name__, url_prefix='/drivers')
 
 @driversBP.route('/', methods=['GET', 'POST'])
 def drivers():
+	selected_list = []
+	selected_columns = []
+	column_dict = {	"forename": "Name",
+			"surname": "Surname",
+			"nationality": "Nationality",
+			"number": "Number",
+			"driverRef": "Reference", 
+			"code": "Code",
+			"dob": "Date of Birth"
+			}
+	if request.method == "POST":
+		for key, value in request.form.items():
+			if value == 'on':
+				selected_list.append(key)
+				selected_columns.append(column_dict[key])
+				
+		if len(selected_list) != 0:
+			print(selected_columns)
+			context = current_app.db.getFilteredDrivers(selected_list)	
+		
+		else:
+			context = current_app.db.getDrivers()
+			selected_columns = ["Name", "Surname", "Nationality", "Number"]
+				
+	elif request.method == "GET":
+		context = current_app.db.getDrivers()
+		selected_columns = ["Name", "Surname", "Nationality", "Number"]
 
-	if request.method == 'GET':
-		context = getDrivers()
-		data = {'context': context}
+	data = {'column_dict': column_dict, 'headers': selected_columns, 'context': context}	
 	return render_template('drivers.html', context=data)
-
+	
 @driversBP.route('/<path:driverSlug>')
 def driverDetails(driverSlug):
 	return render_template('driver.html', driverInfo=driverSlug)
