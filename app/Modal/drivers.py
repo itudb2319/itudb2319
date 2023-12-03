88af2294445e9abe08d4d0a3ab79e2d306adebec
Can Çalışkan <81326097+canetizen@users.noreply.github.com>
2023-12-03 19:36:31 +0300

    minor changes

--- app/Modal/drivers.py
+++ app/Modal/drivers.py
@@ -4,20 +4,11 @@ from os.path import join
 def getDrivers():
     with open(join(db.QPATH, 'drivers.sql'), 'r') as f:
         query = f.read()
-        
         data = db.execute_query(query)
     return data
     
-def getFilteredDrivers(self, filtered_columns):
+def getFilteredDrivers(filtered_columns):
 		cond = ', '.join(filtered_columns)
 		header_cond = ", ".join([f"'{item}'" for item in filtered_columns])
-		try:
-			self.cur.execute(f"SELECT driverId, {cond} FROM drivers;")
-			data = self.cur.fetchall()
-
-		except psycopg2.OperationalError as e:
-			click(e)
-			self.conn.rollback()
-			self.conn.close()
-
+		data = db.execute_query(f"SELECT driverId, {cond} FROM drivers;")
 		return data
