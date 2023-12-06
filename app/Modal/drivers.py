186dac49ecc85955026c60a1856ed5b33bb936f6
Yusuf Şahin <yzfsahin@gmail.com>
2023-12-06 10:39:06 +0300

    Fixed two typos

--- app/Modal/drivers.py
+++ app/Modal/drivers.py
@@ -4,11 +4,11 @@ from os.path import join
 def getDrivers():
     with open(join(db.QPATH, 'drivers.sql'), 'r') as f:
         query = f.read()
-        data = db.execute_query(query)
+        data = db.executeQuery(query)
     return data
     
 def getFilteredDrivers(filtered_columns):
 		cond = ', '.join(filtered_columns)
 		header_cond = ", ".join([f"'{item}'" for item in filtered_columns])
-		data = db.execute_query(f"SELECT driverId, {cond} FROM drivers;")
+		data = db.executeQuery(f"SELECT driverId, {cond} FROM drivers;")
 		return data
