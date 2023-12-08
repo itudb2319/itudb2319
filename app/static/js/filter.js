85241fa43dc8c087d101b402815b21e56c62b460
Can Çalışkan <81326097+canetizen@users.noreply.github.com>
2023-12-08 08:40:03 +0300

    filtering purposes

--- app/static/js/filter.js
+++ app/static/js/filter.js
@@ -1,8 +1,49 @@
-function toggleDropdown() {
-    var dropdown = document.getElementById("dropdownContent");
-    if (dropdown.style.display === "block") {
-        dropdown.style.display = "none";
+function toggleDropdownShow() {
+    var dropdownActual = document.getElementById("dropdownContentShow");
+    var dropdownOther = document.getElementById("dropdownContentOrderBy");
+    if (dropdownActual.style.display === "block") {
+        dropdownActual.style.display = "none";
     } else {
-        dropdown.style.display = "block";
+        if (dropdownOther.style.display == "block") {
+            dropdownOther.style.display = "none";
+        }
+        dropdownActual.style.display = "block";
     }
+}
+
+function toggleDropdownOrderBy() {
+    var dropdownOther = document.getElementById("dropdownContentShow");
+    var dropdownActual = document.getElementById("dropdownContentOrderBy");
+    if (dropdownActual.style.display === "block") {
+        dropdownActual.style.display = "none";
+    } else {
+        if (dropdownOther.style.display == "block") {
+            dropdownOther.style.display = "none";
+        }
+        dropdownActual.style.display = "block";
+    }
+}
+
+function combineAndSendForms(action) {
+    var showFormData = new FormData(document.getElementById('showForm'));
+    var orderByFormData = new FormData(document.getElementById('orderByForm'));
+
+    for (var pair of orderByFormData.entries()) {
+        showFormData.append(pair[0], pair[1]);
+    }
+
+    var combinedForm = document.createElement('form');
+    combinedForm.method = 'POST';
+    combinedForm.action = action;
+
+    for (var pair of showFormData.entries()) {
+        var input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = pair[0];
+        input.value = pair[1];
+        combinedForm.appendChild(input);
+    }
+
+    document.body.appendChild(combinedForm);
+    combinedForm.submit();
 }
