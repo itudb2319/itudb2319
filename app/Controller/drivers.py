24d9b65fbeb119d992d81b3a18880255a6c2fc53
Yusuf Emir Sezgin <122892888+JustFallenn@users.noreply.github.com>
2023-12-17 22:59:41 +0300

    Update drivers.py driverDetails -again

--- app/Controller/drivers.py
+++ app/Controller/drivers.py
@@ -1,5 +1,5 @@
 from flask import request, render_template, Blueprint
-from ..Modal.drivers import columnDict, getDrivers
+from ..Modal.drivers import columnDict, getDrivers, getDriverCareer, getDriverPersonal
 from app.Modal.filter import makeFilter
 import math
 driversBP = Blueprint('drivers', __name__, url_prefix='/drivers')
@@ -19,6 +19,15 @@ def drivers():
 	data = {'columnDict': columnDict, 'headers': selectedColumns, 'context': context, 'orderBy': orderBy, 'length': math.ceil(length / 20), 'currentPageNumber': page, 'lastSearched': search}	
 	return render_template('drivers.html', context=data)
 	
-@driversBP.route('/<path:driverSlug>')
-def driverDetails(driverSlug):
-	return render_template('driver.html', driverInfo=driverSlug)
+@driversBP.route('/<path:driverName>_<path:driverSurname>')
+def driverDetails(driverName, driverSurname):
+	params = {'sDriverName': driverName, 'sDriverSurname': driverSurname}
+
+	careerInfo = getDriverCareer(params)
+	personalInfo = getDriverPersonal(params)
+	
+	headerCareer = ['Race', 'Point', 'Wins', 'Podiums', 'DNFs']
+	headerPersonal = ['Name', 'Surname', 'Seasons', 'Date Of Birth', 'Nationality', 'Number']
+
+	data = {'personal': personalInfo, 'career': careerInfo, 'headerP': headerPersonal, 'headerC': headerCareer}
+	return render_template('driver.html', context=data)
