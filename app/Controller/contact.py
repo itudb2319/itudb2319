93370ea100c701782d4d8e248b49be97dd395d55
Yusuf Şahin <yzfsahin@gmail.com>
2023-12-18 03:46:24 +0300

    Update contact.py

--- app/Controller/contact.py
+++ app/Controller/contact.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, Blueprint, current_app
+from flask import request, render_template, Blueprint
 
 contactBP = Blueprint('contact', __name__, url_prefix='/contact')
 
