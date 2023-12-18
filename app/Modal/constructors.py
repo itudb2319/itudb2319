bad8c182b497b6387f61166cf83ea94609fbc55d
Yusuf Şahin <yzfsahin@gmail.com>
2023-12-18 03:49:22 +0300

    Update constructors.py for constructors' details

--- app/Modal/constructors.py
+++ app/Modal/constructors.py
@@ -6,3 +6,17 @@ def getConstructors():
         query = f.read()
         data = db.executeQuery(query)
     return data
+
+def getConstructorDriver(params):
+    with open(join(db.QPATH, 'constructorDriver.sql'), 'r') as f:
+        query = f.read()
+        data = db.executeQuery(query, params)
+
+    return data
+
+def getConstructorCircuit(params):
+    with open(join(db.QPATH, 'constructorCircuit.sql'), 'r') as f:
+        query = f.read()
+        data = db.executeQuery(query, params)
+
+    return data
