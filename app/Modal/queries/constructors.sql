1843ea129101bf2ef9a202f621d7868a70db48a3
Yusuf Şahin <yzfsahin@gmail.com>
2023-12-18 03:53:38 +0300

    Update constructors.sql

--- app/Modal/queries/constructors.sql
+++ app/Modal/queries/constructors.sql
@@ -1,1 +1,3 @@
-SELECT c.name, c.nationality FROM constructors c;
+SELECT c.name, c.nationality, sum(cs.points) AS totalPoints, c.constructorid FROM constructorstandings cs
+    JOIN constructors c ON cs.constructorid = c.constructorid
+    GROUP BY c.constructorid, c.nationality, c.name;
