63051e8f8a9baaddd37d8a6581efed5e6b182584
Yusuf Şahin <yzfsahin@gmail.com>
2024-01-01 20:22:56 +0300

    Update contact.py

--- app/Controller/contact.py
+++ app/Controller/contact.py
@@ -1,9 +1,20 @@
-from flask import request, render_template, Blueprint
+from flask import request, render_template, Blueprint, session, redirect, url_for
+from ..Modal.mail_setup import mail, recipient_email
+from flask_mail import Message
 
-contactBP = Blueprint("contact", __name__, url_prefix=f'/contact')
+contactBP = Blueprint('contact', __name__, url_prefix='/contact')
 
-@contactBP.route('/', methods=['GET', 'POST'])
+@contactBP.route('/', methods=["GET", "POST"])
 def contact():
-	
-	return render_template('contact.html', context={})
+    if request.method == "GET":
+        return render_template('contact.html')
+    else:
+        sender_email = request.form.get('email')
+        subject = request.form.get('subject')
+        message_body = request.form.get('message')
 
+        msg = Message(subject, sender=sender_email, recipients=[recipient_email])
+        msg.body = message_body
+        mail.send(msg)
+
+        return redirect(url_for('contact.contact'))
