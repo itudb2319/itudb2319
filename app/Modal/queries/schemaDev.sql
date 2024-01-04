81a5a7a31af8fb74283388af0bafb0ded3d017e1
Yusuf Şahin <yzfsahin@gmail.com>
2024-01-04 11:34:42 +0300

    final

--- app/Modal/queries/schemaDev.sql
+++ app/Modal/queries/schemaDev.sql
@@ -432,3 +432,15 @@ BEGIN
     ORDER BY age DESC
     LIMIT slimNumber;
 END; $$ LANGUAGE plpgsql;
+
+CREATE FUNCTION user_insertion_logger() RETURNS TRIGGER AS $$
+BEGIN
+   RAISE NOTICE 'A new row with id % and username % has been inserted', NEW.userId, NEW.username;
+   RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER user_logger
+AFTER INSERT ON users
+FOR EACH ROW
+EXECUTE PROCEDURE user_insertion_logger();
